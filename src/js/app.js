d82422dc83bf40f749537529db15a42638a09083
Ryan Palo <ryan@thepalos.com>
2017-11-22 12:57:16 -0800

    First try layout and app component

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,13 @@
-
+var beers = [
+  { name: "8-Ball Stout", year: 1997 },
+  { name: "Affliger X-mas Ale", year: 2004 },
+  { name: "Alaska Smoked Porter", year: 2002 },
+  { name: "Alesmith Anvil", year: 2004 }
+];
 
 var app = new Vue({
   el: "#app",
   data: {
-    words: "Hello, world!"
+    beers
   }
 });
