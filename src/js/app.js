a115e83e94b45142e4a0a231828049e69899890b
Ryan Palo <ryan@thepalos.com>
2017-11-22 16:13:17 -0800

    Hook up contentful api

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 var beers = [
-  { name: "8-Ball Stout", year: 1997 },
-  { name: "Affliger X-mas Ale", year: 2004 },
-  { name: "Alaska Smoked Porter", year: 2002 },
-  { name: "Alesmith Anvil", year: 2004 }
+  // { name: "8-Ball Stout", year: 1997 },
+  // { name: "Affliger X-mas Ale", year: 2004 },
+  // { name: "Alaska Smoked Porter", year: 2002 },
+  // { name: "Alesmith Anvil", year: 2004 }
 ];
 
 var app = new Vue({
@@ -11,3 +11,25 @@ var app = new Vue({
     beers
   }
 });
+
+var req = new XMLHttpRequest();
+var url = "https://cdn.contentful.com/spaces/rw3uot0g1yaa/entries?access_token=fb12d7b8783b59ad5630868bcb5e5e2bb56a632c89790be58bd21fc92b08198e&include=2"
+
+req.onreadystatechange = function() {
+  if (this.readyState === 4 && this.status === 200) {
+    var response = JSON.parse(this.responseText);
+    console.log(response);
+    response.items.forEach( function(item) {
+      var beer = {name: item.fields.name, year: item.fields.year};
+      if (item.fields.image) {
+        beer.image = "https:" + response.includes.Asset.find(function(asset) {
+          return asset.sys.id === item.fields.image.sys.id;
+        }).fields.file.url;
+      }
+      beers.push(beer);
+    });
+  }
+};
+
+req.open("GET", url, true);
+req.send();
